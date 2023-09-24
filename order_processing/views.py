c1f23c2992b11131d16cb8a0187f58da1de4afa4
Parcurcik <pavelbezborodov@bk.ru>
2023-09-25 00:40:29 +0500

    added temaplate

--- order_processing/views.py
+++ order_processing/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from order_processing.models import Food
 
 
 def home(requests):
-    return render(requests, 'index.html')
+    food = Food.objects.all()
+    return render(requests, 'index.html', {'foods': food})
